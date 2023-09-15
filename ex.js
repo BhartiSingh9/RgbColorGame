964ff8f2e11ef6579d2f67522a48142f5416aad2
BhartiSingh9 <108781350+BhartiSingh9@users.noreply.github.com>
2023-09-15 15:02:56 +0530

    es6 edits Please enter the commit message for your changes. Lines starting

--- ex.js
+++ ex.js
@@ -1,141 +1,131 @@
 // Default difficulty level (Medium)
-var numCircles = 4;
+let numCircles = 4;
 
 // Arrays to store colors and the picked color
-var colors = [];
-var pickedColor;
+const colors = [];
+let pickedColor;
 
 // Selecting elements from the HTML document
-var circles = document.querySelectorAll(".circle");
-var colorDisplay = document.querySelector("#color-display");
-var messageDisplay = document.querySelector("#message");
-var h1 = document.querySelector("h1");
-var resetButton = document.querySelector("#reset");
-var modeButtons = document.querySelectorAll(".mode");
-var timerDisplay = document.querySelector("#timer-display");
-var countdown; // Variable to store the countdown interval
+const circles = document.querySelectorAll(".circle");
+const colorDisplay = document.querySelector("#color-display");
+const messageDisplay = document.querySelector("#message");
+const h1 = document.querySelector("h1");
+const resetButton = document.querySelector("#reset");
+const modeButtons = document.querySelectorAll(".mode");
+const timerDisplay = document.querySelector("#timer-display");
+let countdown; // Variable to store the countdown interval
+
+// Simulate an asynchronous operation (e.g., fetching data from an API)
+const simulateAsyncOperation = async () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, 1000); // Simulate a 1-second delay
+    });
+};
 
 // Initialize the game
-init();
+const init = async () => {
+    await simulateAsyncOperation(); // Simulate an asynchronous operation
 
-// Event listener for the "New Colors" button
-resetButton.addEventListener("click", function () {
-    reset();
-    startTimer(10); // Adjust the number of seconds as needed
-});
-
-// Initialize the game
-function init() {
     // Set the initial color display and setup game elements
     colorDisplay.textContent = pickedColor;
     setupCircles();
     setupMode();
     reset();
-}
+};
 
 // Set up event listeners for the colored circles
-function setupCircles() {
-    for (var i = 0; i < circles.length; i++) {
+const setupCircles = () => {
+    circles.forEach((circle, index) => {
         // Set initial circle colors and click event handlers
-        circles[i].style.backgroundColor = colors[i];
-        circles[i].addEventListener("click", circleClickHandler);
-    }
-}
+        circle.style.backgroundColor = genRandomColor();
+        circle.addEventListener("click", circleClickHandler);
+    });
+};
 
 // Set up event listeners for the difficulty mode buttons
-function setupMode() {
-    for (var i = 0; i < modeButtons.length; i++) {
+const setupMode = () => {
+    modeButtons.forEach(button => {
         // Add click event handlers to mode buttons
-        modeButtons[i].addEventListener("click", function () {
-            for (var i = 0; i < modeButtons.length; i++) {
-                modeButtons[i].classList.remove("selected");
-            }
-            this.classList.add("selected");
+        button.addEventListener("click", () => {
+            modeButtons.forEach(btn => btn.classList.remove("selected"));
+            button.classList.add("selected");
 
             // Update difficulty level based on the button clicked
-            if (this.textContent === "Easy") {
-                numCircles = 3;
-            } else if (this.textContent === "Medium") {
-                numCircles = 4;
-            } else {
-                numCircles = 6;
-            }
+            numCircles = button.textContent === "Easy" ? 3 : button.textContent === "Medium" ? 4 : 6;
             reset();
         });
-    }
-}
+    });
+};
 
 // Reset the game
-function reset() {
+const reset = () => {
     // Generate new random colors and reset game state
-    colors = genRandomColors(numCircles);
+    colors.splice(0, colors.length, ...genRandomColors(numCircles));
     pickedColor = chooseColor();
     colorDisplay.textContent = pickedColor;
     h1.style.backgroundColor = "#232323";
     resetButton.textContent = "New Colors";
     messageDisplay.textContent = "";
-    for (var i = 0; i < circles.length; i++) {
-        if (colors[i]) {
-            circles[i].style.display = "block";
-            circles[i].style.backgroundColor = colors[i];
+
+    circles.forEach((circle, index) => {
+        if (index < numCircles) {
+            circle.style.display = "block";
+            circle.style.backgroundColor = colors[index];
         } else {
-            circles[i].style.display = "none";
+            circle.style.display = "none";
         }
         // Add event listeners back to the circles
-        circles[i].addEventListener("click", circleClickHandler);
-    }
+        circle.addEventListener("click", circleClickHandler);
+    });
+
     startTimer(10); // Adjust the number of seconds as needed
-}
+};
 
 // Change the color of all circles and h1 to the given color
-function changeColors(color) {
-    for (var i = 0; i < circles.length; i++) {
-        circles[i].style.backgroundColor = color;
-        h1.style.backgroundColor = color;
-    }
-}
+const changeColors = color => {
+    circles.forEach(circle => {
+        circle.style.backgroundColor = color;
+    });
+    h1.style.backgroundColor = color;
+};
 
 // Choose a random color from the colors array
-function chooseColor() {
-    var random = Math.floor(Math.random() * colors.length);
+const chooseColor = () => {
+    const random = Math.floor(Math.random() * colors.length);
     return colors[random];
-}
+};
 
 // Generate an array of random colors
-function genRandomColors(num) {
-    var arr = [];
-    for (var i = 0; i < num; i++) {
-        arr.push(makeColor());
-    }
-    return arr;
-}
+const genRandomColors = num => Array.from({ length: num }, genRandomColor);
 
 // Generate a random RGB color string
-function makeColor() {
-    var r = Math.floor(Math.random() * 256);
-    var g = Math.floor(Math.random() * 256);
-    var b = Math.floor(Math.random() * 256);
-    return "rgb(" + r + ", " + g + ", " + b + ")";
-}
+const genRandomColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+};
 
 // Display the modal with a given message
-function showModal(message) {
-    var modal = document.getElementById("myModal");
+const showModal = message => {
+    const modal = document.getElementById("myModal");
     modal.style.display = "block";
-    var modalContent = document.querySelector(".modal-content p");
+    const modalContent = document.querySelector(".modal-content p");
     modalContent.textContent = message;
-}
+};
 
 // Start the timer with the given number of seconds
-function startTimer(seconds) {
-    var remainingTime = seconds;
+const startTimer = seconds => {
+    let remainingTime = seconds;
     timerDisplay.textContent = remainingTime;
 
     if (countdown) {
         clearInterval(countdown);
     }
 
-    countdown = setInterval(function () {
+    countdown = setInterval(() => {
         remainingTime--;
         timerDisplay.textContent = remainingTime;
 
@@ -145,16 +135,16 @@ function startTimer(seconds) {
             showModal("Time's up! Play Again");
 
             // Remove event listeners for the circles
-            for (var i = 0; i < circles.length; i++) {
-                circles[i].removeEventListener("click", circleClickHandler);
-            }
+            circles.forEach(circle => {
+                circle.removeEventListener("click", circleClickHandler);
+            });
         }
     }, 1000);
-}
+};
 
 // Handle the click event on a circle
-function circleClickHandler() {
-    var clickedColor = this.style.backgroundColor;
+const circleClickHandler = function() {
+    const clickedColor = this.style.backgroundColor;
     if (clickedColor === pickedColor) {
         messageDisplay.textContent = "Correct";
         resetButton.textContent = "Play Again";
@@ -165,18 +155,21 @@ function circleClickHandler() {
         this.style.backgroundColor = "#232323";
         messageDisplay.textContent = "Try again";
     }
-}
+};
 
 // Add click event listeners to circles
-for (var i = 0; i < circles.length; i++) {
-    circles[i].addEventListener("click", circleClickHandler);
-}
+circles.forEach(circle => {
+    circle.addEventListener("click", circleClickHandler);
+});
 
 // "Play Again" button click event handler
-document.getElementById("playAgainButton").addEventListener("click", function () {
-    var modal = document.getElementById("myModal");
+document.getElementById("playAgainButton").addEventListener("click", () => {
+    const modal = document.getElementById("myModal");
     modal.style.display = "none";
-    
+
     // Reset the game state
     reset();
 });
+
+// Initialize the game when the page loads
+window.addEventListener("load", init);
